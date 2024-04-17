297e1023c603b613560686768b20dc12bed16311
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-04-17 18:49:33 +0000

    2/2

---
+++ game.js
@@ -0,0 +1,25 @@
+/* global Phaser */
+
+// Copyright (c) 2024 Calista.E ALL Rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Apr 2024
+// This is the Phaser3 game configuration file
+
+/** 
+ * Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    }
+  }
+  // set backgroud color
+  backgroundColor: 0x5f6e7a,
+  scale: {}
+}
