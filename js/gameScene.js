beef2deca204355b19c9368d27084ac87a18df1d
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-05-21 18:17:14 +0000

    jghjm

--- js/gameScene.js
+++ js/gameScene.js
@@ -75,7 +75,7 @@ class GameScene extends Phaser.Scene {
     this.createAlien()
 
     // Collisions between missiles and aliens
-    this.physics.add.collider(this,this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
       alienCollide.destroy()
       missileCollide.destroy()
       this.sound.play("explosion")
