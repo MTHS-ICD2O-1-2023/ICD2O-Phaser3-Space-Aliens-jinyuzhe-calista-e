a7da4f8393b11fe891cb491c2761901558cc4222
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-05-23 18:05:26 +0000

    wfwsfbafb

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,6 +10,7 @@
  * This class is the Game Scene.
  */
 class GameScene extends Phaser.Scene {
+
   // create an alien
   createAlien () {
     const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
@@ -24,9 +25,11 @@ class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: "gameScene" })
 
-    this.background = null
     this.ship = null
     this.fireMissile = false
+    this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = { font: "65px Arial", fill: "#ffffff", align:"center"}
   }
 
   /**
@@ -36,7 +39,7 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   init(date) {
-    this.cameras.main.setBackgroundColor("ffffff")
+    this.cameras.main.setBackgroundColor("#0x5f6e7a")
   }
 
   /**
@@ -65,6 +68,8 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
+    this.scoreText = this.add.text(10, 10, "Score: " + this.score.toString(), this.scoreTextStyle)
+
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
     // create a group for the missile
@@ -79,6 +84,8 @@ class GameScene extends Phaser.Scene {
       alienCollide.destroy()
       missileCollide.destroy()
       this.sound.play("explosion")
+      this.score = this.score + 1
+      this.scoreText.setText("Score: " + this.score.toString())
       this.createAlien()
       this.createAlien()
     }.bind(this))
